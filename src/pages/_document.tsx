08a28e0aee53712d177e53c0feb4db36d3a9d6eb
choi1five <cwo1401@gmail.com>
2023-04-06 22:35:57 +0900

    Feat: Modal 컴포넌트 추가 및 _document 모달 관련 root div 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
       <body>
+        <div id="backdrop-root"></div>
+        <div id="modal-root"></div>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
