c837401cf0adc41843ba382571c50b8b3988b3d6
choi1five <cwo1401@gmail.com>
2023-04-07 22:38:37 +0900

    Refactor: input을 textarea로 재사용하기 위해 styled로 분리

--- src/components/common/Input/Input.tsx
+++ src/components/common/Input/Input.tsx
@@ -3,20 +3,19 @@ import styled from '@emotion/styled';
 import { flexbox } from '@/styles/mixin';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
-  label: string;
+  label?: string;
 }
 
-const Input = ({ id, label, ...attributes }: Props) => {
-  console.log(attributes);
+const InputWithLabel = ({ id, label, ...attributes }: Props) => {
   return (
     <Wrapper>
-      <label htmlFor={id}>{label}</label>
-      <input id={id} name={id} {...attributes} />
+      {label && <label htmlFor={id}>{label}</label>}
+      <Input id={id} name={id} {...attributes} />
     </Wrapper>
   );
 };
 
-export default Input;
+export default InputWithLabel;
 
 const Wrapper = styled.div`
   ${flexbox('column', 'center', 'flex-start')};
@@ -27,19 +26,19 @@ const Wrapper = styled.div`
     font-weight: bold;
     margin-bottom: 20px;
   }
+`;
 
-  & input {
-    width: 100%;
-    padding: 16px 20px;
-    background: ${({ theme }) => theme.black};
-    color: ${({ theme }) => theme.white};
-    border: 2px solid ${({ theme }) => theme.gray};
-    border-radius: 5px;
-    outline: none;
-    text-align: end;
+export const Input = styled.input<Props>`
+  width: 100%;
+  padding: '16px 20px';
+  background: ${({ theme }) => theme.black};
+  color: ${({ theme }) => theme.white};
+  border: 2px solid ${({ theme }) => theme.gray};
+  text-align: end;
+  border-radius: 5px;
+  outline: none;
 
-    &:focus {
-      border-color: ${({ theme }) => theme.white};
-    }
+  &:focus {
+    border-color: ${({ theme }) => theme.white};
   }
 `;
